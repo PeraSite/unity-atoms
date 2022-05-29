f2d9e204b11b22b282ec138b0d9961bf2e8f8218
PeraSIte <pretocki3@gmail.com>
2022-05-29 17:38:33 +0900

    Cleanup Code

--- Packages/Core/Runtime/Variables/AtomVariable.cs
+++ Packages/Core/Runtime/Variables/AtomVariable.cs
@@ -169,7 +169,6 @@ namespace UnityAtoms
         {
             _oldValue = InitialValue;
             _value = InitialValue;
-
             _changedInstantiatedAtRuntime = false;
             _changedWithHistoryInstantiatedAtRuntime = false;
         }
